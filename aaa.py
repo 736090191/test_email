10c3f2e10a9b47e5f8d758233fe05c6869431698
dp <736090191@qq.com>
2020-02-23 15:26:06 +0800

    update

---
+++ aaa.py
@@ -0,0 +1,28 @@
+from functools import reduce
+__author__ = 'Administrator'
+
+# def is_odd(n):
+#     return n % 2 == 1
+#
+# s = filter(is_odd, [1, 2, 4, 5, 6, 9, 10, 15])
+# for i in s:
+#     print(i)
+
+# d = {"a":1,"b":2}
+# print((list(d.keys())))
+# print(d.keys())
+
+
+# a = ['name','age']
+# b = ['xiaobai',18]
+# d = ".."
+# d.join(zip(a, b))
+# print(d)
+# x="aaa"
+# isinstance(x, str)
+
+import time
+f = open("111.txt","a")
+# print(type(str(time.time())))
+f.write(str(time.time()) + "\n")
+f.close()
